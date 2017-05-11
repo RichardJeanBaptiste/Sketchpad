ddc44db2b8a0f51cf282be28cc06363f82e6e8e4
Richard Jean-Baptiste <Richinbk1@gmail.com>
2017-05-11 19:05:03 -0400

    made a canvas to work with

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    function createCanvas(){
+    	var canvas = ("<canvas></canvas>");
+     for(var i=0; i < 16; i++){
+     $("body").append(canvas);
+      }
+    }; 
+    
+    
+ 
+   
+
+});
